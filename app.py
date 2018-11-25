96db3de88e5ce4d60a1979386554ea43b904ed58
tkunisada <tkunisada@infragistics.com>
2018-11-25 13:11:41 -0500

    Made for Python 3.6, Flask 1.02

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, __version__
+from flask_pymongo import PyMongo
+import sys
+
+app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/flask_quickstart'
+mongo = PyMongo(app)
+
+
+@app.route('/')
+def hello_world():
+    dogs = mongo.db.dogs.find()
+    count = dogs.count()
+    if count < 1:
+        mongo.db.dogs.insert_many([
+            {'name': 'Bella', 'age': 1},
+            {'name': 'Lucy', 'age': 5},
+            {'name': 'Daisy', 'age': 10}
+        ])
+
+    versions = f'Python {sys.version.title()} | Flask {__version__}'
+
+    return render_template('index.html', count=count, dogs=dogs, versions=versions)
+
+
+if __name__ == '__main__':
+    app.run()
